394dbc246206e275c14df258074a15a0d1878b85
Alok <iosdevnative@gmail.com>
2022-05-15 23:18:10 +0200

    move RemoteFeedImageDataLoader to app target

---
+++ InfiniteScrolling/ImageLoader/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,42 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  InfiniteScrolling
+//
+//  Created by Alok Sinha on 2022-05-15.
+//
+
+import Foundation
+
+final class RemoteFeedImageDataLoader: FeedImageDataLoader {
+    
+    typealias Result = FeedImageDataLoader.Result
+    
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void)   {
+        
+        client.get(form: url) { result in
+            switch result {
+                
+            case .success( let data ):
+                if  !data.isEmpty {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(Error.invalidData))
+                }
+            case .failure:
+                completion(.failure(Error.invalidData))
+            }
+        }
+    }
+}
+
--- InfiniteScrollingTests/ImageLoader/RemoteFeedImageDataLoaderTests.swift
+++ InfiniteScrollingTests/ImageLoader/RemoteFeedImageDataLoaderTests.swift
@@ -8,40 +8,6 @@
 import XCTest
 @testable import InfiniteScrolling
 
-final class RemoteFeedImageDataLoader: FeedImageDataLoader {
-    
-    typealias Result = FeedImageDataLoader.Result
-    
-    private let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    func loadImageData(from url: URL, completion: @escaping (Result) -> Void)   {
-        
-        client.get(form: url) { result in
-            switch result {
-                
-            case .success( let data ):
-                if  !data.isEmpty {
-                    completion(.success(data))
-                } else {
-                    completion(.failure(Error.invalidData))
-                }
-            case .failure:
-                completion(.failure(Error.invalidData))
-            }
-        }
-    }
-}
-
-
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
     
     func testEmptyData() {
